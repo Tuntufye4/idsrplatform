c707e8ccbd19ad1299478826e9bb27f3176df92e
Tuntufye4 <tuntumwanyongo@gmail.com>
2025-09-11 20:36:25 +0200

    idstplatform

--- src/api/api.js
+++ src/api/api.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000/api/", // adjust if needed
+  baseURL: "https://idsr-backend.onrender.com/api/", // adjust if needed
   headers: {
     "Content-Type": "application/json",
   },
 });
-
+   
 export default api;
